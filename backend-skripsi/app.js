22e58c993589a4955f443eb7a74ab6e782b605e5
danielputraclt <danielputraclt97@gmail.com>
2022-07-06 00:11:59 +0700

    connect mongodb

--- backend-skripsi/app.js
+++ backend-skripsi/app.js
@@ -1,18 +1,28 @@
-import express  from 'express';
+import express from 'express';
 import dotenv from 'dotenv';
-import logger from 'morgan';
+import mongoose from 'mongoose';
 
 import indexRouter from './routes/index.js';
 
 const env = dotenv.config().parsed;
 var app = express();
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 
+//connect database mongoDB
+mongoose.connect(`${env.MONGODB_URI}${env.MONGODB_HOST}:${env.MONGODB_PORT}`, {
+    dbName: env.MONGODB_DB_NAME,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+
 app.listen(env.APP_PORT, () => {
-    console.log(`Server port ${env.APP_PORT}`);
-})
+    console.log(`Server is running on port ${env.APP_PORT}`);
+});
