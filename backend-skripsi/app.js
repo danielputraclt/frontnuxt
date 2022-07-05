287a99bbd7c84f83ec2a5d71de8e2e6fedf59071
danielputraclt <danielputraclt97@gmail.com>
2022-07-05 23:24:59 +0700

    dotenv

--- backend-skripsi/app.js
+++ backend-skripsi/app.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express  from 'express';
+import dotenv from 'dotenv';
+import logger from 'morgan';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+import indexRouter from './routes/index.js';
 
+const env = dotenv.config().parsed;
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-module.exports = app;
+app.listen(env.APP_PORT, () => {
+    console.log(`Server port ${env.APP_PORT}`);
+})
